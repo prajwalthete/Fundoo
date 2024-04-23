92d85b689bc8f6053a7c2bb858fe6e263c594767
prajwal <prajwalthete98@gmail.com>
2024-04-23 11:14:48 +0530

    [Prajwal] Added :media query for signup page

--- src/app/components/displaynotes/displaynotes.component.ts
+++ src/app/components/displaynotes/displaynotes.component.ts
@@ -1,28 +1,4 @@
-/**
- * The `DisplaynotesComponent` is a component that displays a list of notes. It handles various actions on the notes, such as archiving, unarchiving, trashing, untrashing, and deleting. It also provides functionality to open an edit note dialog and update the note.
- *
- * The component receives the following inputs:
- * - `notesData`: an array of `NoteObj` objects representing the notes to be displayed.
- * - `container`: a string representing the container of the notes (e.g., "archive", "trash").
- * - `iconAction`: a string representing the action to be performed on the note (e.g., "archive", "unarchive").
- *
- * The component emits the following output:
- * - `updateNotesList`: an event emitter that emits an object with the action performed on the note and the updated note data.
- *
- * The component also manages the following state:
- * - `showColorPicker`: a boolean flag to toggle the visibility of the color picker.
- * - `searchQuery`: a string representing the current search query.
- * - `subscription`: a subscription to the `currSearchQuery` observable from the `DataService`.
- *
- * The component provides the following methods:
- * - `handleIconsClick(action: string, note: NoteObj)`: handles the click events on the note icons and performs the corresponding action (archive, unarchive, trash, untrash, delete, or update color).
- * - `toggleColorPicker()`: toggles the visibility of the color picker.
- * - `selectColor(color: string, noteID: number)`: selects a color for the note (not implemented).
- * - `openEditNote(noteObj: NoteObj)`: opens the edit note dialog and updates the note if the dialog is closed with an updated note.
- * - `openSnackBar(message: string)`: opens a snackbar to display messages (not implemented).
- *
- * The component also handles the lifecycle events `ngOnInit` and `ngOnDestroy` to subscribe and unsubscribe to the `currSearchQuery` observable.
- */
+
 import {
   Component,
   EventEmitter,
@@ -33,8 +9,8 @@ import {
   TemplateRef,
   ViewChild,
 } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon'; // Importing MatIconRegistry to register custom SVG icons
-import { DomSanitizer } from '@angular/platform-browser'; // Importing DomSanitizer to bypass security and sanitize SVG icons
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import {
   REMINDER_ICON,
   COLLABRATOR_ICON,
@@ -46,39 +22,51 @@ import {
   UNARCHIVE_ICON,
   RESTORE_ICON,
   DELETE_FOREVER_ICON,
-} from 'src/assets/svg-icons'; // Importing custom SVG icon paths
-import { MatDialog } from '@angular/material/dialog'; // Importing MatDialog for opening dialogs
-import { MatSnackBar } from '@angular/material/snack-bar'; // Importing MatSnackBar for showing snackbar messages
-import { NoteObj } from 'src/assets/type'; // Importing NoteObj interface
-import { NoteService } from 'src/app/services/noteService/note.service'; // Importing NoteService for managing notes
-import { EditnoteComponent } from '../editnote/editnote.component'; // Importing EditnoteComponent for editing notes
-import { DataService } from 'src/app/services/dataService/data.service'; // Importing DataService for managing data
-import { Subscription } from 'rxjs'; // Importing Subscription for managing subscriptions
+} from 'src/assets/svg-icons';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoteObj } from 'src/assets/type';
+import { NoteService } from 'src/app/services/noteService/note.service';
+import { EditnoteComponent } from '../editnote/editnote.component';
+import { DataService } from 'src/app/services/dataService/data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-displaynotes', // Selector for the component
-  templateUrl: './displaynotes.component.html', // Template file for the component
-  styleUrls: ['./displaynotes.component.scss'], // Stylesheet file for the component
+  selector: 'app-displaynotes',
+  templateUrl: './displaynotes.component.html',
+  styleUrls: ['./displaynotes.component.scss'],
 })
 export class DisplaynotesComponent implements OnInit, OnDestroy {
-  @Input() notesData: NoteObj[] = []; // Input property to receive an array of NoteObj objects representing notes to be displayed
-  @Input() container: string = ''; // Input property to receive a string representing the container of the notes
-  @ViewChild('unarchiveButton') unarchiveButton!: TemplateRef<any>; // Reference to a template element
-  @Output() updateNotesList = new EventEmitter<{ action: string; data: NoteObj; }>(); // Output property to emit events to parent component
+  @Input() notesData: NoteObj[] = [];
+  @Input() container: string = '';
+  @ViewChild('unarchiveButton')
+  unarchiveButton!: TemplateRef<any>;
+  // Output to emit events to parent component
+  @Output() updateNotesList = new EventEmitter<{
+    action: string;
+    data: NoteObj;
+  }>();
 
-  showColorPicker: boolean = false; // Flag to toggle the visibility of the color picker
-  searchQuery: string = ''; // String representing the current search query
-  subscription!: Subscription; // Subscription to the currSearchQuery observable from the DataService
+  // Input to receive note data from parent component
+  //@Input() note!: { title: string, description: string, noteID: number, color: string, archive: boolean };
+
+  // Input to receive icon action from parent component
+  @Input() iconAction!: string;
+
+  // Flag to toggle color picker visibility
+  showColorPicker: boolean = false;
+  searchQuery: string = '';
+  subscription!: Subscription;
 
   constructor(
-    iconRegistry: MatIconRegistry, // Dependency injection for MatIconRegistry to register custom SVG icons
-    sanitizer: DomSanitizer, // Dependency injection for DomSanitizer to bypass security and sanitize SVG icons
-    public dialog: MatDialog, // Dependency injection for MatDialog to open dialogs
-    public snackBar: MatSnackBar, // Dependency injection for MatSnackBar to show snackbar messages
-    private noteService: NoteService, // Dependency injection for NoteService to manage notes
-    private dataService: DataService // Dependency injection for DataService to manage data
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    private noteService: NoteService,
+    private dataService: DataService
   ) {
-    // Register SVG icons using MatIconRegistry
+    // Register SVG icons
     iconRegistry.addSvgIconLiteral(
       'reminder-icon',
       sanitizer.bypassSecurityTrustHtml(REMINDER_ICON)
@@ -87,101 +75,132 @@ export class DisplaynotesComponent implements OnInit, OnDestroy {
       'edit-icon',
       sanitizer.bypassSecurityTrustHtml(EDIT_ICON)
     );
-    // Register other custom SVG icons in a similar manner
+    iconRegistry.addSvgIconLiteral(
+      'archive-icon',
+      sanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'coll-icon',
+      sanitizer.bypassSecurityTrustHtml(COLLABRATOR_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'Color-Palatte-icon',
+      sanitizer.bypassSecurityTrustHtml(COLOR_PALATTE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'img-icon',
+      sanitizer.bypassSecurityTrustHtml(IMG_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'more-icon',
+      sanitizer.bypassSecurityTrustHtml(MORE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'unarchive-icon',
+      sanitizer.bypassSecurityTrustHtml(UNARCHIVE_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'delete-icon',
+      sanitizer.bypassSecurityTrustHtml(DELETE_FOREVER_ICON)
+    );
+    iconRegistry.addSvgIconLiteral(
+      'restore-icon',
+      sanitizer.bypassSecurityTrustHtml(RESTORE_ICON)
+    );
   }
-
-  ngOnInit(): void {
-    // Subscribe to currSearchQuery observable from the DataService and update searchQuery
+  ngOnInit(): void { // ngOnInit lifecycle hook method
+    // Subscribing to currSearchQuery observable to receive search query updates
     this.subscription = this.dataService.currSearchQuery.subscribe(
-      (res) => (this.searchQuery = res)
+      (res) => (this.searchQuery = res) // Updating searchQuery when observable emits a new value
     );
   }
 
-  // Handle click events on icons and perform corresponding actions
-  handleIconsClick(action: string, note: NoteObj) {
-    if (action === 'archive') {
-      // If action is archive, call archiveNoteCall from NoteService and emit updateNotesList event
+  handleIconsClick(action: string, note: NoteObj) { // Method to handle click events on icons
+    // Handling different actions based on the provided action string
+    if (action === 'archive') { // If action is archive
+      // Calling archiveNoteCall method of NoteService to archive the note
       this.noteService.archiveNoteCall(note.noteId || 0).subscribe(
         () => {
+          // Emitting event to notify parent component about the archived note
           this.updateNotesList.emit({ action: 'archive', data: note });
         },
-        (err: any) => console.log(err)
+        (err: any) => console.log(err) // Logging error if archive operation fails
       );
-    } else if (action === 'unarchive') {
-      // If action is unarchive, call archiveNoteCall from NoteService and emit updateNotesList event
+    } else if (action === 'unarchive') { // If action is unarchive
+      // Calling archiveNoteCall method of NoteService to unarchive the note
       this.noteService.archiveNoteCall(note.noteId || 0).subscribe(
         () => {
+          // Emitting event to notify parent component about the unarchived note
           this.updateNotesList.emit({ action: 'unarchive', data: note });
         },
-        (err: any) => console.log(err)
+        (err: any) => console.log(err) // Logging error if unarchive operation fails
       );
-    } else if (action === 'trash') {
-      // If action is trash, call trashNoteCall from NoteService and emit updateNotesList event
+    } else if (action === 'trash') { // If action is trash
+      // Calling trashNoteCall method of NoteService to move the note to trash
       this.noteService.trashNoteCall(note.noteId || 0).subscribe(
         () => {
+          // Emitting event to notify parent component about the trashed note
           this.updateNotesList.emit({ action: 'trash', data: note });
         },
-        (err: any) => console.log(err)
+        (err: any) => console.log(err) // Logging error if trash operation fails
       );
-    } else if (action === 'untrash') {
-      // If action is untrash, call trashNoteCall from NoteService and emit updateNotesList event
+    } else if (action === 'untrash') { // If action is untrash
+      // Calling trashNoteCall method of NoteService to untrash the note
       this.noteService.trashNoteCall(note.noteId || 0).subscribe(
         () => {
+          // Emitting event to notify parent component about the untrashed note
           this.updateNotesList.emit({ action: 'untrash', data: note });
         },
-        (err: any) => console.log(err)
+        (err: any) => console.log(err) // Logging error if untrash operation fails
       );
-    } else if (action === 'delete') {
-      // If action is delete, call deleteNoteCall from NoteService and emit updateNotesList event
+    } else if (action === 'delete') { // If action is delete
+      // Calling deleteNoteCall method of NoteService to delete the note
       this.noteService.deleteNoteCall(note.noteId || 0).subscribe(
         () => {
+          // Emitting event to notify parent component about the deleted note
           this.updateNotesList.emit({ action: 'delete', data: note });
         },
-        (err: any) => console.log(err)
+        (err: any) => console.log(err) // Logging error if delete operation fails
       );
-    } else {
-      // If action is color update, call updateNoteCall from NoteService and emit updateNotesList event
+    } else { // If action is not any of the predefined actions
+      // Calling updateNoteCall method of NoteService to update the note with new color
       this.noteService
         .updateNoteCall(note.noteId || 0, { ...note, colour: action })
         .subscribe(
           () => {
+            // Emitting event to notify parent component about the updated note's color
             this.updateNotesList.emit({
               action: 'colour',
               data: { ...note, colour: action },
             });
           },
-          (err: any) => console.log(err)
+          (err: any) => console.log(err) // Logging error if update operation fails
         );
     }
   }
 
-  // Toggle the visibility of the color picker
-  toggleColorPicker() {
-    this.showColorPicker = !this.showColorPicker;
+  toggleColorPicker() { // Method to toggle color picker visibility
+    this.showColorPicker = !this.showColorPicker; // Toggling the value of showColorPicker property
   }
 
-  // Method to select a color for the note (not implemented)
-  selectColor(color: string, noteID: number) {
-    // This method is intentionally left empty
+  selectColor(color: string, noteID: number) { // Method to select a color for the note
+    // This method is intentionally left empty as it's not implemented
   }
 
-  // Method to open the edit note dialog and update the note if the dialog is closed with an updated note
-  openEditNote(noteObj: NoteObj) {
-    // Open the edit note dialog with MatDialog
-    let dialogRef = this.dialog.open(EditnoteComponent, { data: noteObj });
-    // Subscribe to the dialog's afterClosed event to get the updated note
-    dialogRef.afterClosed().subscribe((updatedNote: NoteObj) => {
-      if (updatedNote) {
-        // If an updated note is received, call updateNoteCall from NoteService to update the note
+  openEditNote(noteObj: NoteObj) { // Method to open edit note dialog
+    let dialogRef = this.dialog.open(EditnoteComponent, { data: noteObj }); // Opening the edit note dialog
+    dialogRef.afterClosed().subscribe((updatedNote: NoteObj) => { // Subscribing to dialog close event
+      if (updatedNote) { // If an updated note object is received
+        // Making API call to update the note
         this.noteService
           .updateNoteCall(updatedNote.noteId || 0, updatedNote)
           .subscribe(
             () => {
-              // Emit an event to notify the parent component about the updated note
+              // Emitting event to notify parent component about the updated note
               this.updateNotesList.emit({ action: ' ', data: updatedNote });
             },
             (err: any) => {
-              console.log(err);
+              console.log(err); // Logging error if update operation fails
               // Handle error if needed
             }
           );
@@ -189,13 +208,11 @@ export class DisplaynotesComponent implements OnInit, OnDestroy {
     });
   }
 
-  // Method to open a snackbar to display messages (not implemented)
-  openSnackBar(message: string) {
-    // This method is intentionally left empty
+  openSnackBar(message: string) { // Method to open snackbar to display messages
+    // This method is intentionally left empty as it's not implemented
   }
 
-  // Method to unsubscribe from the currSearchQuery observable when the component is destroyed
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  ngOnDestroy() { // ngOnDestroy lifecycle hook method
+    this.subscription.unsubscribe(); // Unsubscribing from the subscription to prevent memory leaks
   }
 }
--- src/app/services/httpService/http.service.ts
+++ src/app/services/httpService/http.service.ts
@@ -1,53 +1,63 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Importing HttpClient and HttpHeaders from '@angular/common/http' for making HTTP requests
+import { Injectable } from '@angular/core'; // Importing Injectable decorator from '@angular/core' for creating injectable services
+import { Observable } from 'rxjs/internal/Observable'; // Importing Observable from 'rxjs/internal/Observable' for handling asynchronous data streams
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Providing the service at the root level
 })
 export class HttpService {
-  private BaseUrl: string = 'https://localhost:7178/api/';
+  private BaseUrl: string = 'https://localhost:7178/api/'; // Setting the base URL for API requests
 
+  // Creating an authentication header with JWT token from local storage
   private authHeader = new HttpHeaders({
     Accept: 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('AuthToken') || ''}`,
+    Authorization: `Bearer ${localStorage.getItem('AuthToken') || ''}`, // Retrieving JWT token from local storage
   });
-  constructor(public httpClient: HttpClient) { }
 
+  constructor(public httpClient: HttpClient) { } // Constructor injecting HttpClient for making HTTP requests
+
+  // Method for sending a login request
   loginUser(endpoint: string, data: Object): Observable<any> {
-    return this.httpClient.post<any>(this.BaseUrl + endpoint, data);
+    return this.httpClient.post<any>(this.BaseUrl + endpoint, data); // Making a POST request to login endpoint
   }
 
+  // Method for sending a registration request
   registerUser(endpoint: string, data: Object): Observable<any> {
-    return this.httpClient.post<any>(this.BaseUrl + endpoint, data);
+    return this.httpClient.post<any>(this.BaseUrl + endpoint, data); // Making a POST request to register endpoint
   }
+
+  // Method for getting all notes
   getAllNotes(endpoint: string): Observable<any> {
-    return this.httpClient.get<any>(this.BaseUrl+endpoint, {headers: this.authHeader});
+    return this.httpClient.get<any>(this.BaseUrl + endpoint, { headers: this.authHeader }); // Making a GET request to retrieve all notes
   }
 
+  // Method for adding a new note
   addNote(endpoint: string, data: Object): Observable<any> {
-    return this.httpClient.post<any>(this.BaseUrl+endpoint, data, {headers: this.authHeader});
+    return this.httpClient.post<any>(this.BaseUrl + endpoint, data, { headers: this.authHeader }); // Making a POST request to add a new note
   }
 
+  // Method for moving a note to trash
   trashNote(endpoint: string): Observable<any> {
-    return this.httpClient.patch<any>(this.BaseUrl+endpoint, {}, {headers: this.authHeader});
+    return this.httpClient.patch<any>(this.BaseUrl + endpoint, {}, { headers: this.authHeader }); // Making a PATCH request to move a note to trash
   }
 
+  // Method for archiving a note
   archiveNote(endpoint: string): Observable<any> {
-    return this.httpClient.patch<any>(this.BaseUrl+endpoint, {}, {headers: this.authHeader});
+    return this.httpClient.patch<any>(this.BaseUrl + endpoint, {}, { headers: this.authHeader }); // Making a PATCH request to archive a note
   }
 
-  deleteNote (endpoint: string): Observable<any> {
-    return this.httpClient.delete<any>(this.BaseUrl +endpoint,{headers: this.authHeader});
-
+  // Method for deleting a note
+  deleteNote(endpoint: string): Observable<any> {
+    return this.httpClient.delete<any>(this.BaseUrl + endpoint, { headers: this.authHeader }); // Making a DELETE request to delete a note
   }
+
+  // Method for updating a note
   updateNote(endpoint: string, data: Object): Observable<any> {
-    return this.httpClient.put<any>(this.BaseUrl + endpoint, data, { headers: this.authHeader });
+    return this.httpClient.put<any>(this.BaseUrl + endpoint, data, { headers: this.authHeader }); // Making a PUT request to update a note
   }
   
+  // Method for sending a forgot password request
   forgotPassword(endpoint: string, data: Object): Observable<any> {
-    return this.httpClient.post<any>(this.BaseUrl + endpoint, data);
+    return this.httpClient.post<any>(this.BaseUrl + endpoint, data); // Making a POST request to forgot password endpoint
   };
-
-
 }
